66a639572b053f001c58305c324cfef3a4fe5332
willieLjohnson <liwa.johnson@gmail.com>
2019-05-27 22:00:00 -0400

    Initialize project

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+  BoxCollider2D collider;
+
+  void Start()
+  {
+    collider = GetComponent<BoxCollider2D>();
+  }
+}
