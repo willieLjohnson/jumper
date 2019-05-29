1b44d104f14a5d2b0131eb0aeeef182176ed1393
willieLjohnson <liwa.johnson@gmail.com>
2019-05-29 12:51:42 -0400

    Create collisioninfo struct to hold context of collisions on every frame

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -17,6 +17,7 @@ public class Controller2D : MonoBehaviour
 
   BoxCollider2D collider;
   RaycastOrigins raycastOrigins;
+  public CollisionInfo collisions;
 
   void Start()
   {
@@ -27,6 +28,8 @@ public class Controller2D : MonoBehaviour
   public void Move(Vector3 velocity)
   {
     UpdateRayCastOrigins();
+    collisions.Reset(;
+
     if (velocity.x != 0)
       HorizontalCollisions(ref velocity);
     if (velocity.y != 0)
@@ -52,6 +55,9 @@ public class Controller2D : MonoBehaviour
       {
         velocity.x = (hit.distance - skinWidth) * directionX;
         rayLength = hit.distance;
+
+        collisions.left = directionX == -1;
+        collisions.left = directionX == 1;
       }
     }
   }
@@ -73,6 +79,9 @@ public class Controller2D : MonoBehaviour
       {
         velocity.y = (hit.distance - skinWidth) * directionY;
         rayLength = hit.distance;
+ 
+        collisions.below = directionY == -1;
+        collisions.above = directionY == 1;
       }
     }
   }
@@ -104,5 +113,17 @@ public class Controller2D : MonoBehaviour
   {
     public Vector2 topLeft, topRight;
     public Vector2 bottomLeft, bottomRight;
+  }
+
+  public struct CollisionInfo
+  {
+    public bool above, below;
+    public bool left, right;
+
+    public void Reset()
+    {
+      above = below = false;
+      left = right = false;
+    }
   }
 }
