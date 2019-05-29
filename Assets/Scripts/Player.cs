9e7876fb7e12b7e5d3b411940de4026f480530e9
willieLjohnson <liwa.johnson@gmail.com>
2019-05-29 11:54:20 -0400

    Get input for player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
+  float movesSpeed = 6;
   float gravity = -20;
   Vector3 velocity;
 
@@ -17,6 +18,9 @@ public class Player : MonoBehaviour
 
   void Update()
   {
+    Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+    velocity.x = input.x * movesSpeed;
     velocity.y += gravity * Time.deltaTime;
     controller.Move(velocity * Time.deltaTime);
   }
