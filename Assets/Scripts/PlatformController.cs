43d0090bcff3b9895b31432c79044093fdc3af01
willieLjohnson <liwa.johnson@gmail.com>
2019-05-29 22:49:07 -0400

    Platform now moves player properly when on top and moving downward/horizontally

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -65,7 +65,7 @@ public class PlatformController : RaycastController
         Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
         rayOrigin += Vector2.up * (horizontalRaySpacing * i);
         RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
-  
+
         if (hit)
         {
           if (!movedPassengers.Contains(hit.transform))
@@ -73,6 +73,30 @@ public class PlatformController : RaycastController
             movedPassengers.Add(hit.transform);
             float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
             float pushY = 0;
+
+            hit.transform.Translate(new Vector3(pushX, pushY));
+          }
+        }
+      }
+    }
+
+    // Passenger on top of a horizontal or downward platform
+    if (directionY == -1 || velocity.y == 0 && velocity.x != 0)
+    {
+      float rayLength = skinWidth * 2 ;
+
+      for (int i = 0; i < verticalRayCount; i++)
+      {
+        Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+        if (hit)
+        {
+          if (!movedPassengers.Contains(hit.transform))
+          {
+            movedPassengers.Add(hit.transform);
+            float pushX = velocity.x;
+            float pushY = velocity.y;
 
             hit.transform.Translate(new Vector3(pushX, pushY));
           }
