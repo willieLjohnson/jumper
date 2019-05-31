8e80887efa6d0425590a033dcd0a25fe02364b9d
willieLjohnson <liwa.johnson@gmail.com>
2019-05-31 14:02:35 -0400

    Jumping off maxsloap while sliding propels you

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -89,7 +89,18 @@ public class Player : MonoBehaviour
 
     if (controller.collisions.below)
     {
-      velocity.y = maxJumpVelocity;
+      if (controller.collisions.slidingDownMaxSlope)
+      {
+        if (directionalInput.x != -Mathf.Sign(controller.collisions.slopeNormal.x))
+        {
+          velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
+          velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
+        }
+      }
+      else
+      {
+        velocity.y = maxJumpVelocity;
+      }
     }
   }
 
