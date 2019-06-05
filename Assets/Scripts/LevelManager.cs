2f5cd5389ec281114546b6ec53af7995f8693d7c
willieLjohnson <liwa.johnson@gmail.com>
2019-06-05 09:32:03 -0400

    Add level manager to check for winning state

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemies.childCount == 0) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
