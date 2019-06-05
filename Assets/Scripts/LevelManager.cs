903df57bbfa1b5ea196b1bd01ffbd70eef234df4
willieLjohnson <liwa.johnson@gmail.com>
2019-06-05 09:45:29 -0400

    Add winning sound

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,18 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public Transform enemies;
-    // Start is called before the first frame update
-    void Start()
-    {
-  
-    }
+  public Transform enemies;
 
-    // Update is called once per frame
-    void Update()
+  public AudioSource audioSource;
+  public AudioClip levelUpClip;
+
+  private bool levelTransitioning = false;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (enemies.childCount == 0 && !levelTransitioning) 
     {
-        if (enemies.childCount == 0) {
-            SceneManager.LoadScene(0);
-        }
+      levelTransitioning = true;
+      audioSource.PlayOneShot(levelUpClip);
+      Invoke("Win", 1f);
     }
+  }
+
+  private void Win()
+  {
+    SceneManager.LoadScene(0);
+  }
 }
