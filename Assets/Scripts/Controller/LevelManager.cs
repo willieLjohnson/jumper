7cadf12b008718755b32c24f8563010022145766
willieLjohnson <liwa.johnson@gmail.com>
2019-06-06 21:02:32 -0400

    Update to have persistant level manager

--- Assets/Scripts/Controller/LevelManager.cs
+++ Assets/Scripts/Controller/LevelManager.cs
@@ -5,31 +5,106 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-  public Transform enemies;
+  public static LevelManager Instance { get; private set; }
 
-  public AudioSource audioSource;
   public AudioClip levelUpClip;
 
-  private bool levelTransitioning = false;
-  // Start is called before the first frame update
-  void Start()
-  {
+  private int currentScene = 0;
 
+  private bool levelTransitioning = false;
+  private bool newLevel = false;
+
+  Transform enemies;
+  GameObject player;
+
+  [HideInInspector]
+  public AudioSource audioSource;
+
+  void Awake()
+  {
+    if (Instance == null)
+    {
+      Instance = this;
+      DontDestroyOnLoad(gameObject);
+    }
+    else
+    {
+      Destroy(gameObject);
+    }
+  }
+
+  private void Start()
+  {
+    SetupLevel();
+  }
+
+  private void SetupLevel()
+  {
+    enemies = GameObject.Find("Enemies").transform;
+    player = GameObject.FindGameObjectWithTag("Player");
+
+    levelTransitioning = false;
+    newLevel = false;
+
+    if (audioSource == null)
+    {
+      audioSource = GetComponent<AudioSource>();
+    }
   }
 
   // Update is called once per frame
   void Update()
   {
-    if (enemies.childCount == 0 && !levelTransitioning) 
+    if (newLevel)
+    {
+      SetupLevel();
+      return;
+    }
+
+    if (enemies.childCount == 0 && !levelTransitioning)
     {
       levelTransitioning = true;
       audioSource.PlayOneShot(levelUpClip);
       Invoke("Win", 1f);
     }
+
+    if (player == null && !levelTransitioning)
+    {
+      levelTransitioning = true;
+      // audioSource.PlayOneShot(levelUpClip);
+      Invoke("Lose", 1f);
+    }
   }
 
-  private void Win()
+  public void Win()
   {
-    SceneManager.LoadScene(0);
+    if (currentScene + 1 > SceneManager.sceneCount)
+    {
+      GoToFirstScene();
+    }
+    else
+    {
+      GoToNextScene();
+    }
+  }
+
+  public void Lose()
+  {
+    GoToFirstScene();
+  }
+
+
+  public void GoToFirstScene()
+  {
+    currentScene = 0;
+    newLevel = true;
+    SceneManager.LoadScene(currentScene);
+  }
+
+  public void GoToNextScene()
+  {
+    currentScene++;
+    newLevel = true;
+    SceneManager.LoadScene(currentScene);
   }
 }
